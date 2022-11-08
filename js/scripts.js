b310b362e59464a4138795b687199feac3935782
Enki <enkimaru@hotmail.com>
2022-11-08 19:18:31 -0300

    Implement Special Fusions

--- js/scripts.js
+++ js/scripts.js
@@ -11,29 +11,36 @@ const colorList = [{"color": "Red", "maxValue": 35},
  {"color":"Black", "maxValue": 35}, {"color":"Yellow", "maxValue": 33}, 
  {"color":"Option", "maxValue": 35}];
 
- const specialFusionList = [1, 4]
 
 const resultDiv = document.getElementById('fusionResult');
 const firstCardList = document.getElementById('firstCard');
 const secondCardList = document.getElementById('secondCard');
 
-var cardList = {};
+var originalCardList = {};
+var specialFusionsList = {};
 
 async function getCardList() {
-    const cardList = await fetch('https://raw.githubusercontent.com/Enkimaru/DigimonCardFusions/color-fusion/db/cardList.json')
+    return await fetch('https://raw.githubusercontent.com/Enkimaru/DigimonCardFusions/color-fusion/db/cardList.json')
                    .then((response) => response.json())
-    return cardList;
+}
+
+async function getSpecialFusionList() {
+    return await fetch('https://raw.githubusercontent.com/Enkimaru/DigimonCardFusions/color-fusion/db/specialFusions.json')
+                   .then((response) => response.json())
 }
 
 
 async function buildPage() {
-    cardList = await getCardList();
+    originalCardList = await getCardList();
+    specialFusionsList = await getSpecialFusionList();
 
-    cardList.forEach(card => {
+    originalCardList.forEach(card => {
             const cardName = `${card.id} - ${card.name}`
             firstCardList.appendChild(new Option(cardName, card.id));
             secondCardList.appendChild(new Option(cardName, card.id));
     });
+
+    fuseCards();
     
 }
  
@@ -55,7 +62,7 @@ function fuseCards() {
 }
 
 function getCard(id) {
-    return cardList.find(card => card.id == id);
+    return originalCardList.find(card => card.id == id);
 }
 
 function colorFusion (firstCard, secondCard){ 
@@ -71,13 +78,20 @@ function cardFusion (firstCard, secondCard){
         resultCost = maxValue;
     }
 
-	const sameColorCards = cardList.filter((card) => card.color === newColor);
+    let specialFusion = checkForSpecialFusion(firstCard, secondCard);
+    if (specialFusion.length > 0) {
+        return new Array(getCard(specialFusion[0].resultCard));
+    }
 
-	return sameColorCards.filter((card) => card.costValue === resultCost);
+	let cardListResult = originalCardList.filter((card) => card.color === newColor);
+    cardListResult = filterSameCardResult(firstCard, cardListResult);
+    cardListResult = filterSameCardResult(secondCard, cardListResult);
+
+	return findResultCostCards(cardListResult, resultCost);
 }
 
 function findColorIndex(card) {
-     return colorList.findIndex(function(item, i){
+     return colorList.findIndex(function(item){
         return item.color === card.color
     });
 }
@@ -86,3 +100,23 @@ function findColorMaxValue(colorName) {
     return colorList.find(color => color.color == colorName).maxValue;
 }
 
+function filterSameCardResult(card, cardList) {
+    if(card.color !== 'Option') {
+        cardList = cardList.filter(element => element != card);
+    }
+    return cardList;
+}
+
+function findResultCostCards(cardList, resultCost) {
+    let resultList = [];
+    while(resultList.length === 0){
+        resultList = cardList.filter((card) => card.costValue === resultCost);
+        resultCost++;
+    }
+    return resultList;
+}
+
+function checkForSpecialFusion(firstCard, secondCard) {
+    return specialFusionsList.filter(fusion => (fusion.firstCard === firstCard.id && fusion.secondCard === secondCard.id)
+                    || (fusion.firstCard === secondCard.id && fusion.secondCard === firstCard.id));
+}
