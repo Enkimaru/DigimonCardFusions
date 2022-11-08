b4bfa981956f4e15f939a2f174ff92b450315f6e
Enki <enkimaru@hotmail.com>
2022-11-07 22:46:10 -0300

    add colorFusion and cardFusion

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const colorMatrix =[ 
+["option","green","green","yellow","blue","red"],
+["green","option","yellow","red","green","blue"],
+["black","yellow","option","blue","black","green"],
+["yellow","red","blue","option","red","black"],
+["blue","green","black","red","option","yellow"],
+["red","blue","green","black","yellow","option"]
+]
+
+const colorList = ["red","blue","green","black","yellow","option"];
+
+
+
+function colorFusion (card1, card2){
+	card1Color = colorList.indexOf(card1.color);
+	card2Color = colorList.indexOf(card2.color);
+
+	return colorMatrix[card1Color][card2Color];
+}
+
+function cardFusion (card1, card2){
+	const newColor = colorFusion(card1, card2);
+	const resultCost = card1.material + card2.material;
+
+	const sameColorCards = cardList.filter((card) => card.color === newColor);
+
+	return sameColorCards.filter((card) => card.cost === resultCost);
+}
