d85589876a0173cd12435c8db9773fe03b9aaff9
Enkimaru <enkimaru@hotmail.com>
2022-11-08 20:31:57 -0300

    Update fetchAPI.js

--- js/fetchAPI.js
+++ js/fetchAPI.js
@@ -1,13 +1,13 @@
 const fetchAPI = {
   async getCardList() {
     return await fetch(
-      "https://raw.githubusercontent.com/Enkimaru/DigimonCardFusions/color-fusion/main/db/cardList.json"
+      "https://raw.githubusercontent.com/Enkimaru/DigimonCardFusions/main/db/cardList.json"
     ).then((response) => response.json());
   },
 
   async getSpecialFusionList() {
     return await fetch(
-      "https://raw.githubusercontent.com/Enkimaru/DigimonCardFusions/color-fusion/main/db/specialFusions.json"
+      "https://raw.githubusercontent.com/Enkimaru/DigimonCardFusions/main/db/specialFusions.json"
     ).then((response) => response.json());
   },
 };
